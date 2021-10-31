dfb781610b6673bd95e53135cebaab6af86c4aa6
Erich1141 <62522070+Erich1141@users.noreply.github.com>
2021-10-30 18:48:36 -0700

    added more content to garden calculation

--- sound.js
+++ sound.js
@@ -5,7 +5,23 @@ x[2].addEventListener("mouseover", myFunction);
 x[3].addEventListener("mouseover", myFunction);
 
 function myFunction(ev) {
- var file=document.getElementById('tick')
- file.play();
+  var file = document.getElementById('tick')
+  file.play();
+
+}
+
+function calculate() {
+  var width = document.getElementById("w").value;
+  var height = document.getElementById("h").value;
+  var result = width * height;
+  document.getElementById('result').innerHTML = result;
+
+}
+
+function reset() {
+  width = document.getElementById("w").value="";
+  height = document.getElementById("h").value="";
+  document.getElementById('result').innerHTML = "";
+
 
 }
