98575ac647e6b410864370df1e1880a0cbb69945
Erich1141 <62522070+Erich1141@users.noreply.github.com>
2021-11-06 17:18:34 -0700

    added a lot of content in garden calculations

--- sound.js
+++ sound.js
@@ -1,8 +1,22 @@
 var x = document.getElementsByClassName("sound");
+console.log(x.length)
+
 x[0].addEventListener("mouseover", myFunction);
 x[1].addEventListener("mouseover", myFunction);
 x[2].addEventListener("mouseover", myFunction);
 x[3].addEventListener("mouseover", myFunction);
+x[4].addEventListener("mouseover", myFunction);
+x[5].addEventListener("mouseover", myFunction);
+x[6].addEventListener("mouseover", myFunction);
+x[7].addEventListener("mouseover", myFunction);
+x[8].addEventListener("mouseover", myFunction);
+x[9].addEventListener("mouseover", myFunction);
+x[10].addEventListener("mouseover", myFunction);
+x[11].addEventListener("mouseover", myFunction);
+x[12].addEventListener("mouseover", myFunction);
+x[13].addEventListener("mouseover", myFunction);
+
+
 
 function myFunction(ev) {
   var file = document.getElementById('tick')
@@ -10,18 +24,53 @@ function myFunction(ev) {
 
 }
 
-function calculate() {
-  var width = document.getElementById("w").value;
-  var height = document.getElementById("h").value;
-  var result = width * height;
-  document.getElementById('result').innerHTML = result;
+function calculateSQFT() {
+  var width = document.getElementById("w1").value;
+  var length = document.getElementById("l1").value;
+  var result = width * length;
+  if (width && length) {
+    document.getElementById('result1').innerHTML = `${result} SQ.FT.`
+  } else {
+    alert("Every field must have an input value!")
+  }
+}
+
+function calculateCUFT() {
+  var width = document.getElementById("w2").value;
+  var length = document.getElementById("l2").value;
+  var height = document.getElementById("h2").value;
+  if (width && length && height) {
+    var result = width * length * height;
+    document.getElementById('result2').innerHTML = `${result} CU.FT.`;
+  } else {
+    alert("Every field must have an input value!")
+  }
+}
+
+function calcSodAmount() {
+  var width = document.getElementById("wsod").value;
+  var length = document.getElementById("lsod").value;
+  var result = width * length;
+  var pieces = Math.ceil(result / 8);
+  document.getElementById('resultsod1').innerHTML = `${pieces} Sod Rolls`;
+
+
+
+
+
 
 }
 
 function reset() {
-  width = document.getElementById("w").value="";
-  height = document.getElementById("h").value="";
-  document.getElementById('result').innerHTML = "";
-
+  document.getElementById("w1").value = "";
+  document.getElementById("l1").value = "";
+  document.getElementById("w2").value = "";
+  document.getElementById("l2").value = "";
+  document.getElementById("h2").value = "";
+  document.getElementById("result1").innerHTML = "";
+  document.getElementById("result2").innerHTML = "";
+  document.getElementById("wsod").value = "";
+  document.getElementById("lsod").value = "";
+  document.getElementById('resultsod1').innerHTML = "";
 
 }
