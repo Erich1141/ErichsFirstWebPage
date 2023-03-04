a3176fb9240390c3048746e0d37bec08a80b7e54
Erich1141 <62522070+Erich1141@users.noreply.github.com>
2023-03-04 09:25:12 -0800

    changed covid page

--- Covid19.js
+++ Covid19.js
@@ -1,29 +1,23 @@
 var videoarea = document.querySelector("#videoarea")
 
-var rearangeVideos = true;
+var rearangeVideos = false;
 var objectArray = [{
-    title: "Pfizer Director Concerned Over Women's Reproductive Health After COVID-19 Vaccination",
-    video: "https://rumble.com/embed/v25is2a/?pub=c8ff5"
+    title: "Pfizer Unable To Finish Press Conference As Spokespeople Keep Collapsing",
+    video: "https://www.youtube.com/embed/FsQjnv9Dwco"
   },
   {
-    title: "Jordan Trishton Walker, Director of Research and Development Admitting, 'Pfizer Exploring Mutating Covid19 and Preemptively Developing a Vaccine'",
-    video: "https://rumble.com/embed/v24ih1i/?pub=c8ff5"
+    title: "Robert F. Kennedy, Jr. warns America: 'We all need to resist vaccine tyranny'",
+    video: "https://www.brighteon.com/embed/7bc6967a-ac24-4710-a116-88574d0f7ecf"
   }, {
-    title: "James O'Keefe of Project Veritas Confronting Jordan Trishton Walker, Director of Research for Pfizer, about The Directed Evolution of Covid19 by Pfizer.",
-    video: "https://www.youtube.com/embed/u5n7RRKgDog"
+    title: "Robert Kennedy Jr. and Dr. Judy Mikovitz on The Corruption of The CDC, Dr. Fauci, FDA, NIH, Big Pharma and Vaccines!",
+    video: "https://rumble.com/embed/va5bw5/?pub=c8ff5"
   }, {
-    title: " Pink-Haired Communists Teaching Our Kids!': Trump Reveals Plan To 'Save American Education'",
-    video: "https://www.youtube.com/embed/EdOR880Ribo"
+    title: "Stella Immanuel Demands an Apology!",
+    video: "https://rumble.com/embed/vb3bkd/?pub=c8ff5"
   }, {
-    title: "New Documentary-'Died Suddenly'",
-    video: "https://rumble.com/embed/v1to6s2/?pub=c8ff5"
+    title: "Frontline Doctors Discussing Covid 19 Treatments",
+    video: "https://rumble.com/embed/v84hn9/?pub=c8ff5"
   },
-  {
-    title: "Former Navy pilot shares his encounter with UFO",
-    video: "https://www.youtube.com/embed/mljQRYLM2yU"
-
-  }
-
 
 ];
 createPage(objectArray);
