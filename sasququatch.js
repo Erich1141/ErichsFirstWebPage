df8fbcdc324a1bfb52de26df943fe8a8958aafb7
Erich Rick <Erich1141@gmail.com>
2026-01-13 19:19:58 -0800

    testing a link to render

--- sasququatch.js
+++ sasququatch.js
@@ -1,2 +1,17 @@
 var result=document.styleSheets
 console.dir(result[0].cssRules[2].cssText)
+console.dir(result)
+
+var test=document.getElementById("test")
+test.style.width="50px";
+test.style.height="50px";
+test.style.backgroundColor="green";
+test.textContent="Press Me"
+test.addEventListener("click", () => {
+
+var request= fetch("https://erichs-real-server.onrender.com/test")
+.then(data => data.text())
+.then(text=> console.log(text))
+.catch(console.error)
+
+});
